92423153a9cd021e4a849778c1ddf9e623d45d58
= <=>
2017-09-25 09:58:11 -0600

    Fixing some syntax errors

--- routes/web.php
+++ routes/web.php
@@ -27,11 +27,11 @@ Route::get('/reporting', function(){
 
 
 	return 'Reporting feature';
-})
+});
 
 Route::get('/Reporting2', function(){
 
 
 	return 'Feature Reporting2';
-})
+});
 // More testing of commiting.
